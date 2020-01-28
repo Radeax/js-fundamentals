86220f08b3591f9d4b50c473fc4e2589e096c327
Radeax <joflip@gmail.com>
2020-01-28 01:25:45 -0500

    Added the rest of the hoisted versions

--- hoist.js
+++ hoist.js
@@ -16,6 +16,13 @@ function test() {
 }
 
 // 2 - Hoisted Code
+var needle;
+function test() {
+  var needle = 'magnet';
+  console.log(needle);
+}
+needle = 'haystack';
+test();
 
 // 3 - Original Code
 var brendan = 'super cool';
@@ -26,6 +33,13 @@ function print() {
 console.log(brendan);
 
 // 3 - Hoisted Code
+var brendan;
+function print() {
+  brendan = 'only okay';
+  console.log(brendan);
+}
+brendan = 'super cool';
+console.log(brendan);
 
 // 4 - Original Code
 var food = 'chicken';
@@ -38,6 +52,16 @@ function eat() {
 }
 
 // 4 - Hoisted Code
+var food;
+function eat() {
+  var food;
+  food = 'half-chicken';
+  console.log(food);
+  food = 'gone';
+}
+food = 'chicken';
+console.log(food);
+eat();
 
 // 5 - Original Code
 mean();
@@ -51,6 +75,17 @@ var mean = function () {
 console.log(food);
 
 // 5 - Hoisted Code
+var mean;
+mean();
+console.log(food);
+mean = function () {
+  var food;
+  food = "chicken";
+  console.log(food);
+  food = "fish";
+  console.log(food);
+}
+console.log(food);
 
 // 6 - Original Code
 console.log(genre);
@@ -65,6 +100,18 @@ function rewind() {
 console.log(genre);
 
 // 6 - Hoisted Code
+var genre;
+function rewind() {
+  var genre;
+  genre = "rock";
+  console.log(genre);
+  genre = "r&b";
+  console.log(genre);
+}
+console.log(genre);
+genre = "disco";
+rewind();
+console.log(genre);
 
 // 7 - Original Code
 dojo = "san jose";
@@ -79,6 +126,17 @@ function learn() {
 console.log(dojo);
 
 // 7 - Hoisted Code
+function learn() {
+  var dojo;
+  dojo = "seattle";
+  console.log(dojo);
+  dojo = "burbank";
+  console.log(dojo);
+}
+dojo = "san jose";
+console.log(dojo);
+learn();
+console.log(dojo);
 
 // 8 - Original Code
 console.log(makeDojo("Chicago", 65));
@@ -97,4 +155,17 @@ function makeDojo(name, students) {
 }
 
 // 8 - Hoisted Code
-
+function makeDojo(name, students) {
+  const dojo = {};
+  dojo.name = name;
+  dojo.students = students;
+  if (dojo.students > 50) {
+    dojo.hiring = true;
+  }
+  else if (dojo.students <= 0) {
+    dojo = "closed for now";
+  }
+  return dojo;
+}
+console.log(makeDojo("Chicago", 65));
+console.log(makeDojo("Berkeley", 0));
