a15381e992e87c8bb2f46d808c1a9944b2cfb6de
Radeax <joflip@gmail.com>
2020-04-12 21:37:34 -0400

    Initial Commit - Ninja.js

---
+++ ninja/Ninja.js
@@ -0,0 +1,24 @@
+class Ninja {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.speed = 3;
+        this.strength = 3;
+    }
+
+    sayName() {
+        console.log(this.name);
+    }
+
+    showStats() {
+        console.log(`
+        Name: ${this.name}
+        Strength: ${this.strength}
+        Speed: ${this.speed}
+        Health: ${this.health}`);
+    }
+
+    drinkSake() {
+        this.health += 10;
+    }
+}
